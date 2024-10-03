f639fb115c99171259c402d733d67e97603a777b
IvanCastroPablo <ivancastropablo@hotmail.com>
2024-10-03 23:07:18 +0200

    Add exercise 9 of the fouth module.

--- core-javascript/Ivan_Castro_tema_4/ejercicio9/main.js
+++ core-javascript/Ivan_Castro_tema_4/ejercicio9/main.js
@@ -1,31 +1,42 @@
 // Preguntar un número de números a introducir, después, preguntar por esos números, por último, sacar por pantalla la suma de los mismos.
 
+let numero;
+let listaCifras = [];
+
+// Función con el primer prompt
 function preguntarNumero() {
-    let numero = parseFloat(prompt("¿Cuántos números quieres sumar? "));
-    return numero;
+    numero = parseFloat(prompt("¿Cuántos números quieres sumar? "));
+    return 1;
 }
 
+// Lógica para llenar la array
 function preguntarCifras() {
-    let listaCifras =[]
     for (let i = 0; i < numero; i++){
-        cifra = parseFloat(prompt("Añade cifra: "));
+        let cifra = parseFloat(prompt("Añade cifra: "));
         if (!isNaN(cifra)) {
             listaCifras.push(cifra);
         } else {
             alert("Valor no reconocido. Agur.")
             return;
         }
-    } return listaCifras
+    } return 1;
 }
 
+// Reduciendo la array e sacándola en pantalla
 function sumaCifras(a) {
     let suma = a.reduce(function(acumulador, valorActual) {
         return acumulador + valorActual;
     }, 0);
+    document.write(`La suma de todas las cifras es ${suma}.`);
 }
 
-if (typeof preguntarNumero() == "number") {
-    if ((Array.isArray(preguntarCifras()) == "true")) {
+// Mi bloque lógico favorito. :) Lo voy a usar siempre.
+if (preguntarNumero() == 1 && !isNaN(numero)) {
+    if (preguntarCifras() == 1 && listaCifras.length > 0) {
         sumaCifras(listaCifras)
-    } 
+    } else {
+        document.write("No has añadido valores con los cuales trabajar.")
+    }
+} else {
+    document.write("Valores no reconocidos.")
 }
