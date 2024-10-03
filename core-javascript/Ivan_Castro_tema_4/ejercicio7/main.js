11d55a94139e27464dc80d6f6180dae89f8c9cd8
IvanCastroPablo <ivancastropablo@hotmail.com>
2024-10-03 15:02:09 +0200

    Add exercises 7 and 8 of module 4

---
+++ core-javascript/Ivan_Castro_tema_4/ejercicio7/main.js
@@ -0,0 +1,17 @@
+// Página web ejecutada en bucle infinito que pregunta por unavmultiplicación y te responde con la solución mientras te vuelve a preguntar de nuevo.
+
+function multiplicacionRecursiva() {
+    let multiplicando = parseFloat(prompt("Proporciona multiplicando"));  // Convertir a número
+    let multiplicador = parseFloat(prompt("Proporciona multiplicador"));
+
+    // Verificamos si ambos valores son números válidos
+    if (!isNaN(multiplicando) && !isNaN(multiplicador)) {
+        alert(`${multiplicando} x ${multiplicador} = ${multiplicando * multiplicador}`);
+        multiplicacionRecursiva();  // Llamada recursiva
+    } else {
+        alert("Por favor, introduce números válidos.");
+        multiplicacionRecursiva();  // Reintentar
+    }
+}
+
+multiplicacionRecursiva();
---
+++ core-javascript/Ivan_Castro_tema_4/ejercicio8/main.js
@@ -0,0 +1,26 @@
+// Preguntar un número por input, después, imprimir todos los números impares hasta él
+
+let valor = parseInt(prompt("¡Escoge un número! (tampoco te pases de gordo)"));
+
+function imprimirImpares() {
+    let ristraDeNumeros = "";
+
+    for (let i = 1; i < valor; i++) {
+        if (i % 2 == 0) {
+            ristraDeNumeros += (`${i}, `);
+        } else {
+            continue;
+        }
+    }
+    ristraDeNumeros += ("FIN DE LA RISTRA DE PARES :D")
+    document.write(ristraDeNumeros);
+}
+
+
+if (!isNaN(valor) && valor < 1000) {
+    imprimirImpares();
+} else if (!isNaN(valor) && valor > 1000){
+    alert("Te pasaste con el numerito");
+} else {
+    alert("Te pedí un número, no una paella.");
+}
